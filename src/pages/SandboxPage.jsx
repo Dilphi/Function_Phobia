ad5ff45f8094ef67831eacbfe3dbeee002f7240e
Dilphi <has11092001@gmail.com>
2025-12-05 22:41:11 +0500

    Страница песочницы

---
+++ src/pages/SandboxPage.jsx
@@ -0,0 +1,26 @@
+import { Box, Container, VStack, Heading, Text } from '@chakra-ui/react';
+import CodeEditor from '../components/CodeEditor';
+
+export default function SandboxPage() {
+  return (
+    <Container maxW="container.xl" p={4}>
+      <VStack spacing={6} align="stretch">
+        <Box>
+          <Heading size="lg" color="blue.300" mb={2}>Песочница для программирования</Heading>
+          <Text color="gray.300">Выбери язык программирования и начни писать код. Нажми "Run code" чтобы увидеть результат.</Text>
+        </Box>
+        
+        <Box borderRadius="lg" overflow="hidden" border="1px solid" borderColor="gray.700">
+          <CodeEditor />
+        </Box>
+
+        <Box bg="gray.800" p={4} borderRadius="md" borderLeft="4px solid" borderColor="blue.500">
+          <Text fontSize="sm" color="gray.300">
+            Совет: Используй песочницу для экспериментов с кодом перед тем как 
+            применять его в игре. Здесь ты можешь тестировать идеи без ограничений.
+          </Text>
+        </Box>
+      </VStack>
+    </Container>
+  );
+}
