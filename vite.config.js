d37f93f9df881dbe1bc0d027fbaf3f20b8b90665
Dilphi <has11092001@gmail.com>
2025-12-08 13:44:58 +0500

    Запуск ngrok и библиотеки

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // слушать все интерфейсы
+    allowedHosts: [
+      '49d85e957bfc.ngrok-free.app',
+      // или для любых поддоменов ngrok:
+      '.ngrok-free.app',
+      '.ngrok.io'
+    ]
+  }
 })
